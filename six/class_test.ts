640ec1b60e54cf56794669de234389269532f1a4
Coding Miku <102744661+toper664@users.noreply.github.com>
2024-07-09 14:53:14 +0700

    fix: changed login and logout logic

---
+++ six/class_test.ts
@@ -0,0 +1,12 @@
+import { longWait } from "../global";
+
+Feature('class schedule');
+
+BeforeSuite(({ login }) => {
+    login('six');
+});
+
+Scenario('open class schedule',  ({ I }) => {
+    I.click(locate('a').withText('Kelas').last());
+    I.waitForText('Jadwal Perkuliahan Mahasiswa', longWait);
+});
